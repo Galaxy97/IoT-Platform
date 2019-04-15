a1cc5bb05f5ca76d0e2adec9630ac5a1be4c5cc5
Galaxy97 <Atnyehin@gmail.com>
2019-04-15 22:55:42 +0300

    add write data json

--- src/data_json.h
+++ src/data_json.h
@@ -24,11 +24,32 @@ public:
         {
             StaticJsonDocument<200> doc;
             DeserializationError error = deserializeJson(doc, conf_data);
-            if (error) Serial.println(F("Failed to read file"));
-            ssid = doc["ssid"] | "defolt";  // if doc["ssid"] is none alt "defolt"
+            if (error)
+                Serial.println(F("Failed to read file"));
+            ssid = doc["ssid"] | "defolt"; // if doc["ssid"] is none alt "defolt"
             Serial.println(ssid);
-            
         }
         conf_data.close();
-    }
+    };
+
+    void write_data_json()
+    {
+        File conf_data = SPIFFS.open("/setting.json", "r+");
+        if (!conf_data)
+            Serial.println("Failed to open setting file");
+        else
+        {
+            StaticJsonDocument<200> doc;
+            DeserializationError error = deserializeJson(doc, conf_data);
+            if (error)
+                Serial.println(F("Failed to read file"));
+            doc["ssid"] = "Dynamo";
+            Serial.println(ssid);
+            if (serializeJson(doc, conf_data) == 0)
+            {
+                Serial.println(F("Failed to write to file"));
+            }
+        }
+        conf_data.close();
+    };
 };
--- src/main.cpp
+++ src/main.cpp
@@ -4,15 +4,16 @@
 #include <ESP8266WebServer.h>
 #include <ESP8266HTTPClient.h>
 #include "FS.h"
-
-ESP8266WebServer HTTP(80);
-#include "FileSystem.h"
 #include "data_json.h"
+ESP8266WebServer HTTP(80);
+data_json data;
+#include "FileSystem.h"
+
 
 void setup()
 {
   Serial.begin(115200);
-  data_json data;
+ 
   WiFi.begin("Galaxy", "Xiaominote5");  
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
@@ -23,6 +24,10 @@ void setup()
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
   FS_init();
+  HTTP.on("/change",HTTP_GET, [](){
+    handleFileRead("/");
+    data.write_data_json();
+  } );
   HTTP.begin();
   Serial.println("Server is launch");
   data.load_data_json();
